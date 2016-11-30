4c41e6f62a1efb404d10068e747d78c43903941c
dsahlin <danielsahlin8@gmail.com>
2016-11-30 13:48:49 +0100

    Update startpage.php

--- startpage.php
+++ startpage.php
@@ -3,8 +3,8 @@
 		<div class="middlediv">
 			<h1 class="h1startpage">Search legopiece</h1>
 		
-			<form action="startsida.html">
-				<input class="searchbox" type="text" name="searchbox" value="Search in the database...">
+			<form action="search_database.php" method="GET">
+				<input class="searchbox" type="text" name="searchbox" placeholder="Search in the database...">
 				<input type="submit" value="Search">
 			</form>
 		</div>
