052226bbd87fd281c7f3034035a1e34df9e74fe1
dsahlin <danielsahlin8@gmail.com>
2016-11-30 10:21:08 +0100

    Add files via upload

---
+++ startpage.php
@@ -0,0 +1,13 @@
+<?php include "menu.txt"; ?>
+
+		<div class="middlediv">
+			<h1 class="h1startpage">Search legopiece</h1>
+		
+			<form action="startsida.html">
+				<input class="searchbox" type="text" name="searchbox" value="Search in the database...">
+				<input type="submit" value="Search">
+			</form>
+		</div>
+		
+		</body>
+</html>
