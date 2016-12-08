bc7588a1d12efa74864dc358fc23f90c5540e4ce
Adrian Andersson <adran117@student.liu.se>
2016-12-08 11:22:55 +0100

    Fixed images for sets and bricks, good code!

--- search_database.php
+++ search_database.php
@@ -1,4 +1,6 @@
-<?php 
+<?php 	include "menu.txt";
+		
+echo "<div class='middlediv'>"; 
 
 $connection = mysqli_connect("mysql.itn.liu.se","lego","", "lego");
 
@@ -12,7 +14,7 @@ $keyword = $_GET['searchbox'];
 $bricks = mysqli_query($connection, "SELECT inventory.ItemID, inventory.ColorID, colors.Colorname, parts.Partname 
 FROM inventory, parts, colors WHERE inventory.Extra='N' AND inventory.ItemTypeID='P' 
 AND inventory.ItemID=parts.PartID AND inventory.ColorID=colors.ColorID 
-AND (Partname LIKE '%$keyword%' OR PartID='$keyword')");
+AND (Partname LIKE '%$keyword%' OR PartID='$keyword') ORDER BY ItemID, ColorID DESC" );
 
 
 if(mysqli_num_rows($bricks)==0)
@@ -23,11 +25,11 @@ if(mysqli_num_rows($bricks)==0)
 else if(mysqli_num_rows($bricks)==1)
 {
 
-	$result = mysqli_query($connection, "SELECT inventory.SetID, sets.Setname, sets.Year, colors.Colorname FROM
-										inventory, sets, parts, colors
+	$result = mysqli_query($connection, "SELECT inventory.SetID, sets.Setname, sets.Year FROM
+										inventory, sets, parts
 										WHERE parts.PartID=inventory.ItemID AND inventory.SetID=sets.SetID 
 										AND inventory.Extra='N'
-										AND inventory.ColorID=colors.ColorID AND (Partname LIKE '%$keyword%'
+										AND (Partname LIKE '%$keyword%'
 										OR PartID='$keyword')");
 
 		print("<table>\n<tr>");
@@ -39,7 +41,7 @@ else if(mysqli_num_rows($bricks)==1)
 		}
 		
 		print("</tr>\n");
-		while($row = mysqli_fetch_row($result))
+		while($row = mysqli_fetch_array($result))
 		{
 			print("<tr>");
 			for($i=0; $i<mysqli_num_fields($result); $i++)
@@ -49,21 +51,26 @@ else if(mysqli_num_rows($bricks)==1)
 			}
 				// Determine the file name for the small 80x60 pixels image, with a preference for JPG format.
 			   $prefix = "http://www.itn.liu.se/~stegu76/img.bricklink.com/";
-			   $ItemID = $row['ItemID'];
+			   $SetID = $row['SetID'];
+			   
 			   // Query the database to see which files, if any, are available
 			   $imagesearch = mysqli_query($connection, "SELECT * FROM images WHERE ItemTypeID='S' AND 
-			   ItemID='$ItemID'");
+			   ItemID='$SetID'");
 			   // By design, the query above should return exactly one row.
 			   $imageinfo = mysqli_fetch_array($imagesearch);
-			   if($imageinfo['has_jpg']) { // Use JPG if it exists
-				 $filename = "S/$ItemID.jpg";
-			   } else if($imageinfo['has_gif']) { // Use GIF if JPG is unavailable
-				 $filename = "S/$ItemID.gif";
-			   } else { // If neither format is available, insert a placeholder image
-				 $filename = "noimage_small.png";
+			   if($imageinfo['has_jpg']) // Use JPG if it exists
+			   { 
+					$filename = "S/$SetID.jpg";
+			   } 
+			   else if($imageinfo['has_gif']) // Use GIF if JPG is unavailable
+			   { 
+					$filename = "S/$SetID.gif";
+			   } 
+			   else // If neither format is available, insert a placeholder image
+			   { 
+					$filename = "noimage_small.png";
 			   }
-			   print("<td>$filename</td>");
-			   print("<td><img src=\"$prefix$filename\" alt=\"Part $ItemID\"/></td>");
+			   print("<td><img src=\"$prefix$filename\" alt=\"Part $SetID\"/></td>");
 			   print("</tr>\n");
 		}
 }
@@ -79,17 +86,17 @@ else
 	
 	print("</tr>\n");
 		
-		while($row = mysqli_fetch_row($bricks))
+		while($row = mysqli_fetch_array($bricks))
 		{
 			// Determine the file name for the small 80x60 pixels image, with a preference for JPG format.
 			$prefix = "http://www.itn.liu.se/~stegu76/img.bricklink.com/";
-			$ItemID = $row[0];
-			$ColorID = $row[1];
+			$ItemID = $row['ItemID'];
+			$ColorID = $row['ColorID'];
 			// Query the database to see which files, if any, are available
 			$imagesearch = mysqli_query($connection, "SELECT * FROM images WHERE ItemTypeID='P' AND ItemID='$ItemID' AND ColorID='$ColorID'");
 			// By design, the query above should return exactly one row.
 			$imageinfo = mysqli_fetch_array($imagesearch);
-			
+						
 			print("<tr>");
 			for($i=0; $i<mysqli_num_fields($bricks); $i++)
 			{
@@ -108,19 +115,21 @@ else
 			{ 
 				$filename = "noimage_small.png"; // If neither format is available, insert a placeholder image
 			}
-			print("<td>$filename</td>");
 			print("<td><img src=\"$prefix$filename\" alt=\"Part $ItemID\"/></td>");
-			print("<td>$ItemID</td>");
-			print("<td>$ColorID</td>");
-			/*$Colorname = $row['Colorname'];
-			$Partname = $row['Partname'];
-			print("<td>$Colorname</td>");
-			print("<td>$Partname</td>");
-			*/
+
 			print("</tr>\n");	
 		}
 
 }
 
 		mysqli_close($connection);
+		
+		
 ?>
+
+	</div>
+	</body>
+	</html>
+	
+
+	
