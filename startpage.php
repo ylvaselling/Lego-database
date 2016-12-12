a9df27b02c3abedfdbb60860077c1544d94c5977
Adrian Andersson <adran117@student.liu.se>
2016-12-12 09:40:42 +0100

    försök att få sökrutan på alla sidor

--- startpage.php
+++ startpage.php
@@ -1,13 +1,4 @@
-<?php include "menu.txt"; ?>
-
-		<div class="middlediv">
-			<h1 class="h1startpage">Search legopiece</h1>
-		
-			<form name="searchform" action="search_database.php" method="GET" onsubmit="return validateForm()">
-				<input class="searchbox" type="text" name="searchbox" placeholder="Search in the database...">
-				<input type="submit" value="Search">
-			</form>
-		</div>
-		
-		</body>
+<?php include "menu.txt"; ?>		
+	
+	</body>
 </html>
